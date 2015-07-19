076bf8aa4d28cb39712408a302b234b4dadc2d47
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-07-19 12:49:54 +0300

    database apps all addresses with their employees]

--- Homeworks/DatabaseApps/01.ORMEntityFramework/03.DatabaseSearchQueries/Program.cs
+++ Homeworks/DatabaseApps/01.ORMEntityFramework/03.DatabaseSearchQueries/Program.cs
@@ -1,5 +1,6 @@
 ﻿using _01.DBContextSoftuni;
 using System;
+using System.IO;
 using System.Linq;
 
 
@@ -9,24 +10,35 @@ namespace _03.DatabaseSearchQueries
     {
         static void Main()
         {
-            AllEmpWithProjects(2002);
+            AllAddresses();
         }
 
         public static void AllEmpWithProjects(int year)
         {
             var db = new SoftUniEntities();
 
-            var employees = db.Employees.Where(e => e.Projects.Any(p => p.StartDate.Year.ToString() == year.ToString()));
+            var employees = db.Employees.Where(e => e.Projects.Any(p => p.StartDate.Year >= 2001 && p.StartDate.Year <= 2003));
 
             foreach (var employee in employees)
             {
-                Console.WriteLine("FirstName: " + employee.FirstName);
+                Console.WriteLine("Manager name: " + employee.Employee1.FirstName + " " + employee.Employee1.LastName);
                 foreach (var project in employee.Projects)
                 {
-                    Console.WriteLine("project name: {0}, date: {1}", project.Name,
-                        project.StartDate.ToString("dd-MM-yyyy"));
+                    Console.WriteLine("project name: {0}, start date: {1}, end date: {2}", project.Name,
+                        project.StartDate.ToString("dd-MM-yyyy"), project.EndDate.ToString());
                 }
                 Console.WriteLine();
+            }
+        }
+
+        public static void AllAddresses()
+        {
+            var db = new SoftUniEntities();
+
+            var addresses = db.Addresses.Where(a => a.Employees.Any()).OrderByDescending(a => a.Employees.Count).ThenBy(a => a.Town.Name).Take(10);
+            foreach (var address in addresses)
+            {
+                Console.WriteLine("{0}, {1} - {2} employees", address.AddressText, address.Town.Name, address.Employees.Count);
             }
         }
     }
