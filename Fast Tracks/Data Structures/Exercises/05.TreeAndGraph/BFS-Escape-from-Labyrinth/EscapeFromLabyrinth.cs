e37c866295c4ca26d7b17af3ab83976d23930d3f
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-07-23 21:50:49 +0300

    BFS Find shortest path trees and graphs excersice

--- Fast Tracks/Data Structures/Exercises/05.TreeAndGraph/BFS-Escape-from-Labyrinth/EscapeFromLabyrinth.cs
+++ Fast Tracks/Data Structures/Exercises/05.TreeAndGraph/BFS-Escape-from-Labyrinth/EscapeFromLabyrinth.cs
@@ -1,7 +1,140 @@
-﻿public class EscapeFromLabyrinth
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Escape_from_Labyrinth;
+
+public class EscapeFromLabyrinth
 {
+    private const char VisitedCell = 's';
     public static void Main()
     {
-        // TODO: implement me
+        string shortestPathToExit = FindShortestPatchToExit();
+        if (shortestPathToExit == null)
+        {
+            Console.WriteLine("No exit!");
+        }
+        else if (shortestPathToExit == "")
+        {
+            Console.WriteLine("Start is at the exti.");
+        }
+        else
+        {
+            Console.WriteLine("Shortest exit: " + shortestPathToExit);
+        }
+
+        //ReadLabyrinth();
+    }
+
+    //private static void ReadLabyrinth()
+    //{
+    //    width = int.Parse(Console.ReadLine());
+    //    height = int.Parse(Console.ReadLine());
+    //    labyrinth = new char[height, width];
+    //    for (int row = 0; row < height; row++)
+    //    {
+    //        string widthA = Console.ReadLine().Split("[\s*]");
+    //        labyrinth[row, width] = Convert.ToChar(Console.ReadLine());
+    //    }
+    //}
+
+    private static int width = 9;
+    private static int height = 7;
+
+    private static char[,] labyrinth =
+    {
+        {'*','*','*','*','*','*','*','*','*'},
+        {'*','-','-','-','-','*','*','-','-'},
+        {'*','*','-','*','-','-','-','-','*'},
+        {'*','-','-','*','-','*','-','*','*'},
+        {'*','s','*','-','-','*','-','*','*'},
+        {'*','*','-','-','-','-','-','-','*'},
+        {'*','*','*','*','*','*','*','-','*'},
+                                   
+    };
+
+    static string FindShortestPatchToExit()
+    {
+        var queue = new Queue<Point>();
+        var startPosition = FindStartPosition();
+        if (startPosition == null)
+        {
+            return null;
+        }
+        queue.Enqueue(startPosition);
+        while (queue.Count > 0)
+        {
+            var currentCell = queue.Dequeue();
+            if (IsExit(currentCell))
+            {
+                return TracePathBack(currentCell);
+            }
+            TryDirection(queue, currentCell, "U", 0, -1);
+            TryDirection(queue, currentCell, "R", +1, 0);
+            TryDirection(queue, currentCell, "D", 0, +1);
+            TryDirection(queue, currentCell, "L", -1, 0);
+        }
+
+        return null;
+    }
+
+    private static void TryDirection(Queue<Point> queue, Point currentCell, string direction, int deltaX, int deltaY)
+    {
+        int newX = currentCell.X + deltaX;
+        int newY = currentCell.Y + deltaY;
+        if (newX >= 0 && newX < width && newY >= 0 && newY < height && labyrinth[newY, newX] == '-')
+        {
+            labyrinth[newY, newX] = VisitedCell;
+            var nextCell = new Point()
+            {
+                X = newX,
+                Y = newY,
+                Direction = direction,
+                PreviousPoint = currentCell
+            };
+            queue.Enqueue(nextCell);
+
+        }
+    }
+
+    private static string TracePathBack(Point currentCell)
+    {
+        var path = new StringBuilder();
+        while (currentCell.PreviousPoint != null)
+        {
+            path.Append(currentCell.Direction);
+            currentCell = currentCell.PreviousPoint;
+        }
+        var pathReversed = new StringBuilder(path.Length);
+        for (int i = path.Length - 1; i >= 0; i--)
+        {
+            pathReversed.Append(path[i]);
+        }
+        return pathReversed.ToString();
+    }
+
+    private static bool IsExit(Point currentCell)
+    {
+        bool isOnBorderX = currentCell.X == 0 || currentCell.X == width - 1;
+        bool isOnBorderY = currentCell.Y == 0 || currentCell.Y == height - 1;
+
+        return isOnBorderX || isOnBorderY;
+    }
+
+    private static Point FindStartPosition()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (labyrinth[y, x] == VisitedCell)
+                {
+                    return new Point(){X = x, Y = y};
+                }
+            }
+        }
+
+        return null;
     }
 }
---
+++ Fast Tracks/Data Structures/Exercises/05.TreeAndGraph/BFS-Escape-from-Labyrinth/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escape_from_Labyrinth
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Direction { get; set; }
+        public Point PreviousPoint { get; set; }
+    }
+}
