189a16c229d78f665c51288549157319685b170b
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-08-15 12:56:27 +0300

    overview web service and cloud homewowrk

---
+++ Homeworks/WebServices&Cloud/01.Web Services, SOA, SOAP, WSDL and REST/03.DistanceRestService/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace _03.DistanceRestService.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Homeworks/WebServices&Cloud/01.Web Services, SOA, SOAP, WSDL and REST/03.DistanceRestService/Controllers/PointsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _03.DistanceRestService.Controllers
+{
+    public class PointsController : ApiController
+    {
+        public IHttpActionResult GetDistance(int startX, int startY, int endX, int endY)
+        {
+            double deltaX = startX - endX;
+            double deltaY = startY - endY;
+
+            var distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            return this.Ok(distance);
+        }
+    }
+}
