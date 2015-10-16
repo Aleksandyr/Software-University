e53bed95cf644007827a0f5d378ebd52796e35bd
Aleksandyr <aleksandyr.kamenov@abv.bg>
2015-10-16 16:30:15 +0300

    list tweets in home page

--- Homeworks/ASP.NET-MVC/Twitter-Web-App/Twitter.Web/Controllers/HomeController.cs
+++ Homeworks/ASP.NET-MVC/Twitter-Web-App/Twitter.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿namespace Twitter.Web.Controllers
+﻿using System.Linq;
+using System.Web;
+using Twitter.Web.Models.TweetModels;
+
+namespace Twitter.Web.Controllers
 {
     using System.Web.Mvc;
     using Data.UnitOfWork;
@@ -16,9 +20,21 @@
             
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            if (page < 1)
+            {
+                throw new HttpException(400, "Bad Request");
+            }
+            var tweets = this.TwitterData.Tweets.All()
+                .OrderBy(t => t.TimeStamp)
+                .Select(TweetViewModel.Create)
+                .Skip((page - 1) * 10)
+                .Take(10)
+                .ToList();
+
+            this.ViewBag.Page = page;
+            return View(tweets);
         }
 
         public ActionResult About()
