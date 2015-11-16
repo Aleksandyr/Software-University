b01d873e0e0976eb5615a5bcadda75b61805a557
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-16 14:48:38 +0200

    defining classes homework with first task Persons

---
+++ Homeworks/OOP/01.DefiningClasses/01.Persons/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.Build.Framework;
+
+
+namespace _01.Persons
+{
+    class Person
+    {
+        private string name;
+        
+        private int age;
+        
+        private string email;
+
+        public Person(string name, int age, string email)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Email = email;
+        }
+
+        public Person(string name, int age)
+            : this(name, age, "")
+        {
+        }
+
+        [Required]
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name should be not null or empty!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        [Required]
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 1 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Age should be between [1...100]");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !value.Contains("@"))
+                {
+                    throw new ArgumentNullException("Email should be either empty or valid!");
+                }
+
+                this.email = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string person = "Name: " + this.Name + "\nAge: " + this.Age;
+            
+            if (!string.IsNullOrEmpty(this.Email))
+            {
+                person += "\nEmail: " + this.Email + "\n";
+            }
+
+            return person;
+        }
+    }
+}
---
+++ Homeworks/OOP/01.DefiningClasses/01.Persons/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pesho = new Person("pesho", 22);
+            Console.WriteLine(pesho);
+
+            Console.WriteLine("-------------------");
+
+            Person misho = new Person("misho", 50, "misho@abv.bg");
+            Console.WriteLine(misho);
+        }
+    }
+}
