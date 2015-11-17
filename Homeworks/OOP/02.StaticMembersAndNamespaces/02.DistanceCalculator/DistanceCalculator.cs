2a0ddc6cc30cde43e7829e1087b26ad45c147554
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-17 14:44:50 +0200

    second task distance calculator

---
+++ Homeworks/OOP/02.StaticMembersAndNamespaces/02.DistanceCalculator/DistanceCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace DistanceCalculator
+{
+    using Point3D;
+using System;
+
+    public static class DistanceCalculator
+    {
+        public static double CalculateDistance(Point3D firstPoint, Point3D secondPoint)
+        {
+            double xCalc = Math.Pow(secondPoint.X - firstPoint.X, 2);
+            double yCalc = Math.Pow(secondPoint.Y - firstPoint.Y, 2);
+            double zCalc = Math.Pow(secondPoint.Z - firstPoint.Z, 2);
+
+            double d = Math.Pow(xCalc + yCalc + zCalc, 2);
+
+            return d;
+        }
+    }
+}
---
+++ Homeworks/OOP/02.StaticMembersAndNamespaces/02.DistanceCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DistanceCalculator
+{
+    using Point3D;
+
+    class Program
+    {
+        static void Main()
+        {
+            double distance = DistanceCalculator.CalculateDistance(new Point3D(2,3,4), new Point3D(3,4,5));
+            System.Console.WriteLine(distance);
+        }
+    }
+}
