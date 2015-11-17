e932517aa652da16b109da992e996bd2353973d2
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-17 14:28:20 +0200

    first task point3d

---
+++ Homeworks/OOP/02.StaticMembersAndNamespaces/01.Point3D/Point3D.cs
@@ -0,0 +1,78 @@
+﻿namespace Point3D
+{
+    using System.Text;
+
+    public class Point3D
+    {
+        private static readonly Point3D startPoint;
+
+        private float x;
+        private float y;
+        private float z;
+
+        static Point3D()
+        {
+            Point3D.startPoint = new Point3D(0, 0, 0);
+        }
+
+        public Point3D(float x, float y, float z)
+        {
+            this.X = x;
+            this.y = y;
+            this.Z = z;
+        }
+
+        public float X 
+        { 
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+
+        public float Z
+        {
+            get
+            {
+                return this.z;
+            }
+            set
+            {
+                this.z = value;
+            }
+        }
+
+        public static Point3D StartPoint
+        {
+            get
+            {
+                return startPoint;
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "{";
+            result += string.Format("{0}, {1}, {2}", this.X, this.Y, this.Z);
+            result += "}";
+
+            return result;
+        }
+    }
+}
---
+++ Homeworks/OOP/02.StaticMembersAndNamespaces/01.Point3D/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Point3D
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point3D point = new Point3D(2, 3, 5);
+            System.Console.WriteLine(point);
+            System.Console.WriteLine(Point3D.StartPoint);
+        }
+    }
+}
