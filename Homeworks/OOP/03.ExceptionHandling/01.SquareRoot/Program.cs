99d0ae1c0d3fca0fb8a53e3f345053143b429010
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-18 12:11:41 +0200

    first task square root

---
+++ Homeworks/OOP/03.ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    public class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter your number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Invalid number!");
+                }
+
+                Console.WriteLine("Square root: " + Math.Sqrt(number));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
