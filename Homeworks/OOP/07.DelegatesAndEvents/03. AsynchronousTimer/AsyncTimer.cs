9cf1addabe5521a6b634b3cda842e4b25480ef73
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-26 15:41:33 +0200

    third task async timer

---
+++ Homeworks/OOP/07.DelegatesAndEvents/03. AsynchronousTimer/AsyncTimer.cs
@@ -0,0 +1,30 @@
+﻿namespace AsynchronousTimer
+{
+    using System;
+    using System.Threading;
+
+    public class AsyncTimer
+    {
+        public AsyncTimer(Action t, int tick, int tickInterval)
+        {
+            this.T = t;
+            this.Tick = tick;
+            this.TickInterval = tickInterval;
+        }
+
+        public Action T { get; set; }
+
+        public int Tick { get; set; }
+
+        public int TickInterval { get; set; }
+
+        public void StartTimer()
+        {
+            for (int i = 0; i < this.Tick; i++)
+            {
+                Thread.Sleep(this.TickInterval);
+                T.Invoke();
+            }
+        }
+    }
+}
---
+++ Homeworks/OOP/07.DelegatesAndEvents/03. AsynchronousTimer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AsynchronousTimer
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Action getTime = GetTime;
+
+            var timer = new AsyncTimer(getTime, 10, 1000);
+            timer.StartTimer();
+        }
+
+        public static void GetTime()
+        {
+            var timeNow = DateTime.Now;
+            Console.WriteLine("{0}", timeNow.ToString("HH:mm:ss"));
+        }
+    }
+}
