24e9e73a15c30ef959bef99d63ae41c6d11dfd27
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-11-26 15:05:01 +0200

    first task custom linq extension

---
+++ Homeworks/OOP/07.DelegatesAndEvents/01.CustomLINQExtensionMethods/Extensions.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomLINQExtensionMethods
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> WhereNot<T>(
+            this IEnumerable<T> collection, Predicate<T> filterPredicate)
+        {
+            var matches = new List<T>();
+            foreach (var element in collection)
+            {
+                if (!filterPredicate(element))
+                {
+                    matches.Add(element);
+                }
+            }
+
+            return matches;
+        }
+    }
+}
---
+++ Homeworks/OOP/07.DelegatesAndEvents/01.CustomLINQExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomLINQExtensionMethods
+{
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var elements = new List<int>()
+            {
+                 40, 51, 60, 5, 10, 20, 30,
+            };
+
+            var getOddNums = elements.WhereNot(x => x % 2 == 0);
+            foreach (var num in getOddNums)
+            {
+                System.Console.WriteLine(num);
+            }
+        }
+    }
+}
