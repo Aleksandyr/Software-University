be407bd221090de3ad9c0b3d0fb9b69d6ffeb241
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-08 13:43:41 +0200

    oop lab enviroment system skeleton

---
+++ Exercises/OOP/EnvironmentSystem/Models/Objects/Snowflake.cs
@@ -0,0 +1,32 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    using System.Collections.Generic;
+
+    public class Snowflake : MovingObject
+    {
+        public Snowflake(int x, int y, int width, int height, Point direction)
+            : base(x, y, width, height, direction)
+        {
+            this.ImageProfile = this.GenerateImageProfile();
+            this.CollisionGroup = CollisionGroup.Snowflake;
+        }
+
+        protected virtual char[,] GenerateImageProfile()
+        {
+            return new char[,] { { '*' } };
+        }
+
+        public override void RespondToCollision(CollisionInfo collisionInfo)
+        {
+            var hitObjectGroup = collisionInfo.HitObject.CollisionGroup ;
+            if (hitObjectGroup == CollisionGroup.Ground)
+            {
+            }
+        }
+
+        public override IEnumerable<EnvironmentObject> ProduceObjects()
+        {
+            return new EnvironmentObject[0];
+        }
+    }
+}
