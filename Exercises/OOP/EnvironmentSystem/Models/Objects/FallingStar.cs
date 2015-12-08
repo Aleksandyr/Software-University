0164868be099c33c1081699eb166714c9dca4bce
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-08 15:49:21 +0200

    falling star task

---
+++ Exercises/OOP/EnvironmentSystem/Models/Objects/FallingStar.cs
@@ -0,0 +1,36 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    using System;
+    using System.Collections.Generic;
+
+    class FallingStar : MovingObject
+    {
+        public FallingStar(int x, int y, int width, int height, Point direction)
+            : base(x, y, width, height, direction)
+        {
+            this.ImageProfile = new char[,] { { 'O' } };
+        }
+
+        public override void RespondToCollision(CollisionInfo collisionInfo)
+        {
+            var hitObjectGroup = collisionInfo.HitObject.CollisionGroup;
+            if (hitObjectGroup == CollisionGroup.Ground || hitObjectGroup == CollisionGroup.Explosion)
+            {
+                this.Exists = false;
+            }
+        }
+
+        public override IEnumerable<EnvironmentObject> ProduceObjects()
+        {
+            List<EnvironmentObject> producedObjects = new List<EnvironmentObject>();
+            if (this.Exists)
+            {
+                producedObjects.Add(new Tail(this.Bounds.TopLeft.X - this.Direction.X, this.Bounds.TopLeft.Y - this.Direction.Y));
+                producedObjects.Add(new Tail(this.Bounds.TopLeft.X - 2 * this.Direction.X, this.Bounds.TopLeft.Y - 2 * this.Direction.Y));
+                producedObjects.Add(new Tail(this.Bounds.TopLeft.X - 3 * this.Direction.X, this.Bounds.TopLeft.Y - 3 * this.Direction.Y));
+            }
+
+            return producedObjects;
+        }
+    }
+}
---
+++ Exercises/OOP/EnvironmentSystem/Models/Objects/Tail.cs
@@ -0,0 +1,35 @@
+﻿namespace EnvironmentSystem.Models.Objects
+{
+    using System.Collections.Generic;
+
+    public class Tail : EnvironmentObject
+    {
+        private int lifetime;
+
+        public Tail(int x, int y, int lifetime = 1)
+            : base(x, y, 1, 1)
+        {
+            this.ImageProfile = new char[,] { { '*' } };
+            this.lifetime = lifetime;
+        }
+
+        public override void Update()
+        {
+            this.lifetime--;
+            if (this.lifetime <= 0)
+            {
+                this.Exists = false;
+            }
+        }
+
+        public override void RespondToCollision(CollisionInfo collisionInfo)
+        {
+            
+        }
+
+        public override IEnumerable<EnvironmentObject> ProduceObjects()
+        {
+            return new EnvironmentObject[0];
+        }
+    }
+}
