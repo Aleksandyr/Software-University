156304f3deb6dbf68b817fd2f9e742035bbb1e10
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-01-12 12:32:16 +0200

    sixth homework high quality methods is ready

---
+++ Homeworks/HighQualityCode/06.HighQualityMethods/HighQualityMethods/Methods.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Sides should be positive.");
+            }
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                default: throw new InvalidOperationException("Invalid digit!");
+            }
+        }
+
+        static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException("Elements can not be null or empty!");
+            }
+
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i] > elements[0])
+                {
+                    elements[0] = elements[i];
+                }
+            }
+
+            return elements[0];
+        }
+
+        static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+            else if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+            else if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+            else
+            {
+                throw new ArgumentException("Wrong number format", "format");
+            }
+        }
+
+
+        static double CalcDistance(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = false;
+            isVertical = false;
+
+            if ((y1 == y2) && (x1 == x2))
+            {
+                return 0;
+            }
+
+            if (y1 == y2)
+            {
+                isHorizontal = true;
+            }
+
+            if (x1 == x2)
+            {
+                isVertical = true;
+            }
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student("Peter", "Ivanov", new DateTime(1992, 03, 17), "From Sofia");
+
+            Student stella = new Student("Stella", "Markova", new DateTime(1993, 03, 11), "From Vidin, gamer, high results");
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
---
+++ Homeworks/HighQualityCode/06.HighQualityMethods/HighQualityMethods/Student.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+        private string otherInfo;
+
+        public Student(string firstName, string lastName, DateTime dateOfBirth, string otherInfo)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+            this.OtherInfo = otherInfo;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("First name can not be null or empty!", "value"); 
+                }
+
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.otherInfo;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Last name can not be null or empty!", "value");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return this.dateOfBirth;
+            }
+            set
+            {
+                this.dateOfBirth = value;
+            }
+        }
+
+        public string OtherInfo
+        {
+            get
+            {
+                return this.otherInfo;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Other info can not be null or empty!", "value");
+                }
+
+                this.otherInfo = value;
+            }
+        }
+
+        public bool IsOlderThan(Student other)
+        {
+            bool isOlder = this.DateOfBirth < other.DateOfBirth;
+
+            return isOlder;
+        }
+    }
+}
