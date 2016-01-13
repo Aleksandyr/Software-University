90a29e58e197e51972c8534420dc50dc819c9673
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-01-13 23:02:58 +0200

    compare advanced math operations is ready

--- Homeworks/HighQualityCode/09.CodeTuningAndOptimization/Code-Tuning-and-Optimization-Homework/02.CompareAdvancedMathOper/Program.cs
+++ Homeworks/HighQualityCode/09.CodeTuningAndOptimization/Code-Tuning-and-Optimization-Homework/02.CompareAdvancedMathOper/Program.cs
@@ -1,15 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _02.CompareAdvancedMathOper
+﻿namespace _02.CompareAdvancedMathOper
 {
-    class Program
+    using System;
+    using System.Diagnostics;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            int length = 100000000;
+
+            Console.WriteLine("==Square root==");
+            DisplayExecutionTime(() =>
+            {
+                double a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (double)Math.Sqrt(length);
+                }
+
+                Console.WriteLine("double: ");
+            });
+
+            DisplayExecutionTime(() =>
+            {
+                decimal a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (decimal)Math.Sqrt(length);
+                }
+
+                Console.WriteLine("decimal: ");
+            });
+
+            Console.WriteLine("\n == Natural logarithm == ");
+            DisplayExecutionTime(() =>
+            {
+                double a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (double)Math.Log(length);
+                }
+
+                Console.WriteLine("double: ");
+            });
+
+            DisplayExecutionTime(() =>
+            {
+                decimal a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (decimal)Math.Log(length);
+                }
+
+                Console.WriteLine("decimal: ");
+            });
+
+            Console.WriteLine("\n == sinus == ");
+            DisplayExecutionTime(() =>
+            {
+                double a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (double)Math.Sin(length);
+                }
+
+                Console.WriteLine("double: ");
+            });
+
+            DisplayExecutionTime(() =>
+            {
+                decimal a = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    a = (decimal)Math.Sin(length);
+                }
+
+                Console.WriteLine("decimal: ");
+            });
+        }
+
+        public static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
         }
     }
 }
